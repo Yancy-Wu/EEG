cd418d46fa8a2025c9f9d34f9c8ddac80a61b5d6
红开心竹 <45585540+Yancy-Wu@users.noreply.github.com>
2020-10-13 17:45:30 +0800

    意外错误恢复

--- FrontEnd/src/main.ts
+++ FrontEnd/src/main.ts
@@ -17,10 +17,10 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  // mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   Manager.getInstance().setWindow(mainWindow);
 }
